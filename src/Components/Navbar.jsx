2dd8ac75e07a190269cb385904fb90e2af24fce5
kasulaarunteja <kasulaarunteja@gmail.com>
2023-02-02 16:46:47 +0530

    nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,26 +2,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    // <div className='Navbar'>
-    //     <div className='left'>
-    //         <h3>Webure Technologies</h3>
-    //     </div>
-    //     <div className='right'>
-    //         <div>About us</div>
-    //         <div>Service</div>
-    //         <div>Careers</div>
-    //         <div> ROI Stroies</div>
-    //         <div>Blog</div>
-    //         <div>Contact Us</div>
-    //         <div>
-    //         <button>Let's Talk</button>
-    //         </div>
-    //     </div>
-    // </div>
-
     <div id="container">
       <nav>
-        <div id="logo">webure testtechch</div>
+        <div id="logo">Webure Technologies</div>
         <ul>
           <li>
             <a href="#">About us</a>
