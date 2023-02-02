a755260a3ea793979edffbcc5e80f92662c9f7f4
kasulaarunteja <kasulaarunteja@gmail.com>
2023-02-02 18:40:19 +0530

    home

---
+++ src/Components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <img src="/imgs/main2.jpg" alt="Backgroud" height="100%" />
+      </div>
+      <div className="features">
+        <div className="fe-box">
+          <img src="imgs/slack.webp" alt="companyname" />
+        </div>
+        <div className="fe-box">
+          <img src="imgs/prudential.jpg" alt="companyname" />
+        </div>
+        <div className="fe-box">
+          <img src="imgs/mircosoft.png" alt="companyname" />
+        </div>
+        <div className="fe-box">
+          <img src="imgs/zoover.png" alt="companyname" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
