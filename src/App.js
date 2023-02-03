f7ebdbd4981d19cd9d7ab1ca0c9fbf91a274fb0b
kasulaarunteja <kasulaarunteja@gmail.com>
2023-02-03 11:46:38 +0530

    services

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import Services from './Components/Services';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <Services/>
     </div>
   );
 }
---
+++ src/Components/Services.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="maindiv">
+      <div className="leftside">
+        <span>Services</span>
+        <h4>Our Capabilities</h4>
+        <p>
+          We will bring the breathe of our experience and industry knowledge to
+          help you succed
+        </p>
+      </div>
+      <div id="rightside">
+          <div className="fee-box">
+            <img src="imgs/dfasdfas.png" alt="consult" />
+            <h4>consult</h4>
+            <p>Leverage agile framework to provide a robust high level synopsys overviews</p>
+          </div>
+          <div className="fee-box">
+            <img src="imgs/cread.jpg" alt="cread" />
+            <h4>Create</h4>
+            <p>Bring to the table survival strategies to ensure proactive domination</p>
+          </div>
+          <div className="fee-box">
+            <img src="imgs/asdfasdf.png" alt="collabotate.jpg" />
+            <h4>Collaborate</h4>
+            <p>Grow the holistic world view of disruptive innovation workspace</p>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
